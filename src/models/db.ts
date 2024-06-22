6b777584dda74357800689f2cbc29ec9deb0f796
ebrahim-0 <ebrahimabdelrazik2002@gmail.com>
2024-06-22 23:47:39 +0300

    chore: Add SSL configuration for database connection

--- src/models/db.ts
+++ src/models/db.ts
@@ -6,6 +6,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", async () => {

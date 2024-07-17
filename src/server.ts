9937bb3d7b6fddd3a3b3cd05834db69f337f9ee5
ebrahim-0 <ebrahimabdelrazik2002@gmail.com>
2024-07-17 03:45:40 +0300

    chore: Update API endpoint for Swagger documentation

--- src/server.ts
+++ src/server.ts
@@ -71,7 +71,7 @@ const swaggerOptions = {
 };
 
 const swaggerDocs = swaggerJsdoc(swaggerOptions);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use("/api", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 // ** Database connection
 
